2061d17386481af4fbf5d4e9109e5db976386601
Valentin Duffet <vavadu07@gmail.com>
2021-12-21 14:51:04 +0100

    intégration de la map

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import React from 'react'
 import styled from 'styled-components'
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+
+// Components
+import Map from './Components/Map/Map'
 
 export default function App() {
   return (
-    <ContainerMap>
-      {/* <MapContainer center={[51.505, -0.09]} zoom={10} scrollWheelZoom={false}>
-  <TileLayer
-    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  />
-  <Marker position={[51.505, -0.09]}>
-    <Popup>
-      A pretty CSS3 popup. <br /> Easily customizable.
-    </Popup>
-  </Marker>
-</MapContainer> */}
-    </ContainerMap>
+    <div>
+      coucou
+      <Map/>
+    </div>
   )
 }
-
-const ContainerMap = styled.div`
-  height: 180px;
-  width: 100px;
-`;
-
