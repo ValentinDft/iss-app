386d59c1602838cdf7ea95f00586ead1e1b8abc8
Valentin Duffet <vavadu07@gmail.com>
2021-12-22 16:37:56 +0100

    recupération des datas par le store

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import {  iconISS  } from './Icon';
 import "./Map.css"
 
-export default function Map({positionIss}) {
+import {connect} from 'react-redux';
 
-    const position = [positionIss.latitude, positionIss.longitude]
+function Map(props) {
+
+    const position = [props.position.latitude, props.position.longitude]
 
     return (
         <MapContainer center={position} zoom={4}>
@@ -12,11 +15,15 @@ export default function Map({positionIss}) {
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={position}>
-                <Popup>
-                    Je suis l'ISS
-                </Popup>
-            </Marker>
+            <Marker position={position} icon={ iconISS }></Marker>
         </MapContainer>
     )
 }
+
+function mapStateToProps(state) {
+    return { 
+        position: state.iss
+    }
+}
+
+export default connect(mapStateToProps, null)(Map);
