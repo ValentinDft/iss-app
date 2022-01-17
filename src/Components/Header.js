c14563c81490b1fb6121088b1d985ce52f409f4d
Valentin Duffet <vavadu07@gmail.com>
2022-01-17 17:29:43 +0100

    Animation header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,39 @@
-import React, {useRef} from 'react'
+import React, {useRef, useEffect} from 'react'
 import styled from 'styled-components'
 import { gsap } from "gsap";
+// import { ScrollTrigger } from "gsap/ScrollTrigger"
+// import { ScrollToPlugin } from "gsap/ScrollToPlugin"
+
+// gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
 export default function Header() {
+
+    const titleHeader = useRef();
+
+    const animationTitle = elem => {
+        gsap.fromTo(
+            elem,
+            {
+                opacity: 0,
+                y: -100,
+                zoom: 0,
+            },
+            {
+                opacity: 1,
+                y: 0,
+                zoom: 1,
+                duration: 1.2,
+            }
+        )
+    }
+
+    useEffect(() => {
+        animationTitle(titleHeader.current)
+    }, [])
+
     return (
         <ContainerHeader>
-            <h1>Position de l'ISS</h1>
+            <h1 ref={titleHeader}>Position de l'ISS</h1>
         </ContainerHeader>
     )
 }
